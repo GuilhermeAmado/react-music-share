4cfdee7d4ca54c3b31107e8cb2d230efb24af6ec
Guilherme Amado <guiii@live.com>
2022-04-09 20:25:15 -0300

    add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Grid, GridItem } from '@chakra-ui/react';
 import AddSong from './components/AddSong';
 import Header from './components/Header';
 import Player from './components/Player';
@@ -6,10 +7,18 @@ import SongList from './components/SongList';
 function App() {
   return (
     <>
-      <Header />
-      <AddSong />
-      <SongList />
-      <Player />
+      <Grid templateColumns="repeat(12, 1fr)" gap={6}>
+        <GridItem colSpan={12}>
+          <Header />
+        </GridItem>
+        <GridItem colSpan={7}>
+          <AddSong />
+          <SongList />
+        </GridItem>
+        <GridItem colSpan={5}>
+          <Player />
+        </GridItem>
+      </Grid>
     </>
   );
 }
